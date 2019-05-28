c5f80c16745c618d73e4511ee60275365154bc3a
Altelus <altelusenyodelus@gmail.com>
2019-05-28 11:02:48 +0800

    Update decode_xor.py

--- Cryptopals/Set1/decode_xor.py
+++ Cryptopals/Set1/decode_xor.py
@@ -14,4 +14,4 @@ cipher = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 for i in range(256):
 	print(str(i)+" - "+decode_xor(cipher, hex(i)[2:].zfill(2)))
 
-#message = "Cooking MC's like a pound of bacon at byte 88"
+#message = "Cooking MC's like a pound of bacon" at byte 88

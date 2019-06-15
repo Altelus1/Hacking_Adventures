704c148aacd3406b9bc2c546cc85867f04064cea
Altelus <altelusenyodelus@gmail.com>
2019-06-15 18:38:51 +0800

    Update pkcs7_pad.py

--- Cryptopals/Set2/pkcs7_pad.py
+++ Cryptopals/Set2/pkcs7_pad.py
@@ -2,7 +2,7 @@
 
 def pkcs7_padding(message, block_size):
 
-	pad_value = block_size - len(message)
+	pad_value = block_size - (len(message)%block_size)
 	return message + (chr(pad_value))*pad_value
 	
 

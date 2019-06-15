0927ee3f0d0a422f96899b3a11afd9811e0efc5e
Altelus <altelusenyodelus@gmail.com>
2019-06-16 00:15:40 +0800

    Add files via upload

---
+++ Cryptopals/Set2/aes_cbc_encryptor.py
@@ -0,0 +1,59 @@
+from Crypto.Cipher import AES
+import pkcs7_pad as p7p
+import math
+import base64
+
+def encrypt(aes_obj, message):
+    
+    ciphertext = aes_obj.encrypt(message)
+    return ciphertext
+
+def chop_blocks(string_stream, block_size):
+    no_blocks = int(math.ceil(len(string_stream)/block_size))
+    return [string_stream[i*block_size:(i+1)*block_size] for i in range(no_blocks)]
+
+def xor_two_blocks(block1, block2, block_size):
+    xored_block = b''
+    
+    for i in range(block_size):	
+        xored_block += (block1[i] ^ block2[i]).to_bytes(1, byteorder="big", signed=False)
+
+    return xored_block
+
+IV = b'\x00'*16
+
+filename = "10_plain.txt"
+contents = ""
+key = "YELLOW SUBMARINE"
+block_size = 16
+
+with open(filename, "r") as rf:
+    contents = rf.read()
+
+contents = contents.encode('latin-1')
+aes_obj = AES.new(key, AES.MODE_ECB)
+plain_blocks = chop_blocks(contents, block_size)
+#print(plain_blocks)
+
+plbl_length = len(plain_blocks)
+plain_blocks[plbl_length-1] = p7p.pkcs7_padding(plain_blocks[plbl_length-1], block_size)
+cipher_blocks = []
+cipher_block = IV
+
+for plain_block in plain_blocks:
+    cipher_block = encrypt(aes_obj, xor_two_blocks(plain_block, cipher_block, block_size))
+    cipher_blocks.append(cipher_block.decode('latin-1'))
+
+ciphertext = None
+ciphertext=''.join(cipher_blocks).encode('latin-1')
+ciphertext = base64.b64encode(ciphertext)
+print(ciphertext.decode('latin-1'))
+
+
+
+
+
+
+
+
+

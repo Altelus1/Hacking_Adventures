b8b11f6300da04786b7d71adc81004f2dfc71281
Altelus <altelusenyodelus@gmail.com>
2019-07-01 13:02:00 +0800

    Update aes_cbc_encryptor.py

--- Cryptopals/Set2/aes_cbc_encryptor.py
+++ Cryptopals/Set2/aes_cbc_encryptor.py
@@ -15,24 +15,30 @@ def chop_blocks(string_stream, block_size):
 def xor_two_blocks(block1, block2, block_size):
     xored_block = b''
     
-    for i in range(block_size):	
+    for i in range(block_size): 
         xored_block += (block1[i] ^ block2[i]).to_bytes(1, byteorder="big", signed=False)
 
     return xored_block
 
-def encrypt_cbc(plain_blocks, IV = b'\x00'*16):
-	plbl_length = len(plain_blocks)
-	plain_blocks[plbl_length-1] = p7p.pkcs7_padding(plain_blocks[plbl_length-1], block_size)
-	cipher_blocks = []
-	cipher_block = IV
+def encrypt_cbc(plain_message, key, IV = b'\x00'*16, block_size=16):
+    plain_blocks = chop_blocks(plain_message, block_size)
 
-	for plain_block in plain_blocks:
-		cipher_block = encrypt(aes_obj, xor_two_blocks(plain_block, cipher_block, block_size))
-		cipher_blocks.append(cipher_block.decode('latin-1'))
+    aes_obj = AES.new(key, AES.MODE_ECB)
 
-	ciphertext = None
-	ciphertext=''.join(cipher_blocks).encode('latin-1')
-	ciphertext = base64.b64encode(ciphertext)
+    plbl_length = len(plain_blocks)
+    plain_blocks[plbl_length-1] = p7p.pkcs7_padding(plain_blocks[plbl_length-1], block_size)
+    cipher_blocks = []
+    cipher_block = IV
+
+    for plain_block in plain_blocks:
+        cipher_block = encrypt(aes_obj, xor_two_blocks(plain_block, cipher_block, block_size))
+        cipher_blocks.append(cipher_block.decode('latin-1'))
+
+    ciphertext = None
+    ciphertext=''.join(cipher_blocks).encode('latin-1')
+    ciphertext = base64.b64encode(ciphertext)
+
+    return ciphertext
 
 
 
